9598f3d56a93da4351de8ea7e95afbab2cded12d
Karoly Nagy <dr.karoly.nagy@gmail.com>
2018-09-19 22:03:22 +0100

    FIX: path in tests and added nose command to fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,9 @@
 import nose
 import os
+import sys
+
+sys.path.insert(0, os.path.realpath(os.path.dirname(__file__)))
 
 
 def test(suite):
-    nose.run(argv=['-w', os.path.join('tests', suite), '-vv'])
+    nose.run(argv=['-w', os.path.join('tests', suite), '-vv', '--with-cover', '--cover-package=mysqlkit'])
